0880471ae9a8ae2d0f5acfe2f83a979bb2c68f4e
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-26 20:40:17 -0600

    resume update

--- CreateResume.py
+++
@@ -1,8 +0,0 @@
-import CreateResumeFromTemplate, ShareResume
-import shutil, os, time, pyautogui, subprocess, datetime, boto3
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-
-CreateResumeFromTemplate.all()
-ShareResume.all()
