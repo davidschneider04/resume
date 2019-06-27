62cb6d774ed22d8b8ac71dd6ff6d0b9003f4df5d
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-27 15:51:39 -0600

    resume updating

--- CreateResume.sh
+++ CreateResume.sh
@@ -3,13 +3,13 @@ source /Users/kutch/.local/share/virtualenvs/env_django-ZXcG8Was/bin/activate
 echo "committing updated resume information"
 cd '/Users/kutch/resume'
 git add -A
-git commit -m "resume update"
+git commit -m "resume updating"
 git push
 echo "building formatted resume"
 python CreateResumeFromTemplate.py
 echo "saving formatted resume as PDF"
 python ShareResume.py
 git add -A
-git commit -m "resume update"
+git commit -m "resume updated"
 git push
 echo "saved resume to AWS, committed to GitHub"
