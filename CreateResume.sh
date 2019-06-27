993bb5467ca348cfdd52dd7988efa9f87330a966
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-27 15:33:34 -0600

    resume update

--- CreateResume.sh
+++ CreateResume.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+source /Users/kutch/.local/share/virtualenvs/env_django-ZXcG8Was/bin/activate
 echo "committing updated resume information"
 cd '/Users/kutch/resume'
-git fetch
-source /Users/kutch/.local/share/virtualenvs/env_django-ZXcG8Was/bin/activate
 git add -A
 git commit -m "resume update"
 git push
@@ -10,4 +9,7 @@ echo "building formatted resume"
 python CreateResumeFromTemplate.py
 echo "saving formatted resume as PDF"
 python ShareResume.py
+git add -A
+git commit -m "resume update"
+git push
 echo "saved resume to AWS, committed to GitHub"
