a73ff142e8471cab2a9dd2099451c06e9b60b17d
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-28 11:58:14 -0600

    did svg

--- CreateResume.sh
+++ CreateResume.sh
@@ -9,6 +9,11 @@ echo "building formatted resume"
 python CreateResumeFromTemplate.py
 echo "saving formatted resume as PDF"
 python ShareResume.py
+
+pdfseparate DavidSchneiderResume.pdf -l 1 DavidSchneiderResume1.pdf 
+rm DavidSchneiderResume.pdf
+mv DavidSchneiderResume1.pdf DavidSchneiderResume.pdf
+pdf2svg DavidSchneiderResume.pdf DavidSchneiderResume.svg
 git add -A
 git commit -m "resume updated"
 git push
